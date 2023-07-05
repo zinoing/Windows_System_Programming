64abda2707b8729b679929b1ca61107033a908a1
zinoing <77779979+zinoing@users.noreply.github.com>
2023-07-05 19:26:03 +0900

    Create PartAdder.cpp

---
+++ Chapter07/PartAdder.cpp
@@ -0,0 +1,23 @@
+// PartAdder.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+int _tmain(int argc, TCHAR* argv[]) {
+	if (argc != 3) {
+		return -1;
+	}
+
+	DWORD start = _ttoi(argv[1]);
+	DWORD end = _ttoi(argv[2]);
+
+	DWORD total = 0;
+
+	for (DWORD i = start; i <= end; i++) {
+		total += i;
+	}
+
+	return total;
+}
